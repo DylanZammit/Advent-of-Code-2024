7dd48384d5cc7f42b6bd1bd41c2dc5249a32e2c0
Dylan Zammit <dylanzam@gmail.com>
2024-12-14 16:52:57 +0100

    day 14 shorten

--- solutions/Day 14/PartA.py
+++ solutions/Day 14/PartA.py
@@ -20,31 +20,15 @@ p=9,5 v=-3,-3'''
 
 dat = parse(dat, ints)
 print(truncate(dat, 80))
+
 out = 0
-
 n = 100
-
 w, h = 101, 103
-
 quads = [0, 0, 0, 0]
-
 for px, py, vx, vy in dat:
-    px2 = ( px + vx * n ) % w
-    py2 = ( py + vy * n ) % h
-
-    if px2 < w // 2 and py2 < h // 2:
-        quads[0] += 1
-    if px2 > w // 2 and py2 < h // 2:
-        quads[1] += 1
-    if px2 < w // 2 and py2 > h // 2:
-        quads[2] += 1
-    if px2 > w // 2 and py2 > h // 2:
-        quads[3] += 1
-
-
-    print(px2, py2)
+    px2, py2 = ( px + vx * n ) % w, ( py + vy * n ) % h
+    quads[int(px2 < w // 2) + 2 * int(py2 < h // 2)] += 1
 
 out = prod(quads)
-print(quads)
 print(out)
 submit(out, part="a", day=day, year=year)
