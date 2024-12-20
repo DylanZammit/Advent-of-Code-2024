b876281af5e2e721472083c4cd6860aeb3fe20f9
Dylan Zammit <dylanzam@gmail.com>
2024-12-20 15:57:12 +0100

    day 16 part 2

--- solutions/Day 16/PartB.py
+++ solutions/Day 16/PartB.py
@@ -5,7 +5,7 @@ year, day = 2024, 16
 
 dat = get_data(year=year, day=day, block=True)
 
-# 7036
+# 45
 dat2 = '''###############
 #.......#....E#
 #.#.###.#.###.#
@@ -22,7 +22,7 @@ dat2 = '''###############
 #S..#.....#...#
 ###############'''
 
-# 11048
+# 64
 dat2 = '''#################
 #...#...#...#..E#
 #.#.#.#.#.#.#.#.#
@@ -41,11 +41,54 @@ dat2 = '''#################
 #S#.............#
 #################'''
 
+def A_star_search(problem, h=None):
+    """Search nodes with minimum f(n) = path_cost(n) + h(n) value first."""
+    h = h or problem.h
+    return best_first_search(problem, f=lambda n: n.path_cost + h(n))
+
+
+def get_path_so_far(reached_prev, node):
+    curr_node = node.state
+    final_path = [node]
+    while reached_prev[curr_node][1] is not None:
+        prev_node = reached_prev[curr_node][1]
+        final_path.append(prev_node)
+        curr_node = prev_node.state
+    final_path = final_path[::-1]
+
+    return set([f.state for f in final_path])
+
+
+def best_first_search(problem, f) -> List['Node']:
+    "Search nodes with minimum f(node) value first."
+    best_cost = np.inf
+    best_nodes = []
+    node = Node(problem.initial)
+
+    frontier = PriorityQueue([node], key=f)
+    reached = {problem.initial: node}
+    while frontier:
+        node = frontier.pop()
+        if problem.is_goal(node.state):
+            if best_cost < node.path_cost:
+                return best_nodes
+            best_cost = node.path_cost
+            best_nodes.append(node)
+
+        for child in expand(problem, node):
+            s = child.state
+            if s not in reached or child.path_cost <= reached[s].path_cost:
+                reached[s] = child
+                frontier.add(child)
+    return best_nodes
+
+
 class MyProblem(GridProblem):
     """Problem for searching a grid from a start to a goal location.
     A state is just an (x, y) location in the grid."""
 
-    def is_goal(self, state):        return state[0] == self.goal[0]
+    def is_goal(self, state):
+        return state[0] == self.goal[0]
 
     def actions(self, state):
         loc, direction = state
@@ -73,7 +116,11 @@ print('=' * 80)
 
 sp = MyProblem(grid=dat, initial=(init[0] , East), goal=end)
 res = A_star_search(sp)
-out = res.path_cost
+tiles = set()
+for path in res:
+    print(path_states(path))
+    tiles |= set([x[0] for x in path_states(path)])
+out = len(tiles)
 print(out)
 
-submit(out, part="a", day=day, year=year)
+submit(out, part="b", day=day, year=year)
